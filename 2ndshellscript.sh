a1afb40fe9d463f3b0bbfc4ec6a154bc5a7c8685
saahil1206 <saahil1206@outlook.com>
2025-11-05 10:29:23 +0530

    2 new scripts added

---
+++ 2ndshellscript.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# this is our new file 
+
+name="shyam" 
+
+echo "name is $name and date is $(date)"
+
+echo "enter the user name" 
+
+read username
+
+echo "you have enterd name as $username" 
---
+++ 3rdshellscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+read -p "enter username " username
+#this is a static variable 
+
+name="shyam_sagane"
+
+# this is the use of dynamic variable where user can provide the vaule of variable 
+
+echo "you enterd name $username" 
+
+#this is a argument where you will provide the vaule while execution the file 
+echo "the character in show is $0 and $1"
+
